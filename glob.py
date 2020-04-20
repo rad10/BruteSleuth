99694fd11fbe6bba61b70d643ee8fdffcbd60b18
Nick Cottrell <nrc1mr.e3@gmail.com>
2020-04-20 16:41:06 -0400

    Added lowercase letter iterator

--- glob.py
+++ glob.py
@@ -3,3 +3,32 @@ from sys import argv
 mainString = "test-{0:04}"
 
 
+class LowerAlphaChain:
+    length = 0
+    chr_array = []
+
+    def __init__(self, length: int):
+        self.length = length
+        self.chr_array = ["a" for a in range(length)]
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if (self.chr_array[0] == "{"):
+            self.chr_array = ["a" for a in range(self.length)]
+            raise StopIteration
+
+        curr = "".join(self.chr_array)
+
+        # increment last char
+        self.chr_array[-1] = chr(ord(self.chr_array[-1]) + 1)
+
+        # incrementing modulus chars
+        for i in range(len(self.chr_array) - 1, 0, -1):
+            if (self.chr_array[i] == "{"):
+                self.chr_array[i] = "a"
+                self.chr_array[i - 1] = chr(ord(self.chr_array[i - 1]) + 1)
+
+        return curr
+
