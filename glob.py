ded98c75e4a2b30ec52797bcd8be4de80714ff55
Nick Cottrell <nrc1mr.e3@gmail.com>
2020-04-20 17:02:42 -0400

    Added capitol case generator

--- glob.py
+++ glob.py
@@ -30,5 +30,32 @@ class LowerAlphaChain:
                 self.chr_array[i] = "a"
                 self.chr_array[i - 1] = chr(ord(self.chr_array[i - 1]) + 1)
 
-        return curr
 
+class UpperAlphaChain:
+    length = 0
+    chr_array = []
+
+    def __init__(self, length: str):
+        self.length = length
+        self.chr_array = ["A" for a in range(length)]
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if(self.chr_array[0] == "["):
+            self.chr_array = ["A" for a in range(self.length)]
+            raise StopIteration
+
+        curr = "".join(self.chr_array)
+
+        # increment last character
+        self.chr_array[-1] = chr(ord(self.chr_array[-1]) + 1)
+        #incrementing all characters if need be
+        for i in range(len(self.chr_array) - 1, 0, -1):
+            if(self.chr_array[i] == "["):
+                self.chr_array[i - 1] = chr(ord(self.chr_array[i - 1]) + 1)
+                self.chr_array[i] = "A"
+            else:
+                break
+        return curr
