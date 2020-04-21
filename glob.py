fa6e05587bb81637acb7be103011888d833adb6f
Nick Cottrell <nrc1mr.e3@gmail.com>
2020-04-21 00:47:48 -0400

    Included argparse for all commands and help

--- glob.py
+++ glob.py
@@ -1,4 +1,4 @@
-from sys import argv
+import argparse
 import re
 from itertools import product
 
@@ -181,6 +181,62 @@ def iterative_printer(format_string: str, generators: list, regex: str = ""):
             if bool(re.match(reg_filter, format_string.format(*i))):
                 print(format_string.format(*i))
 
+
 if __name__ == "__main__":
-    setup = init_formatting(argv[1])
-    iterative_printer(*setup)
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+        description="""This program is a string combination creator. You
+        give it a string and place unknowns in python string format using {}.
+        For more capabilties with the formatting, look at pythons official
+        string documentation for more formatting functionallity.""")
+
+    parser.add_argument(
+        "fstring", type=str, metavar="format_string",
+        help="The main string that you use for your wildcard bruteforce string")
+    parser.add_argument(
+        "-r", nargs="?", default="", type=str, metavar="exclusive_regex",
+        help="""This arguement takes all the values returned by the program and
+        only prints values that perfectly match the regular expression given.""")
+
+    parser.epilog = """
+        Description of Formatting
+        This script works with official python formatting. For more information on proper python
+        formatting, see the official documentation[1].
+        This program also works with custom formatting of its own design. This was implemented so
+        that iteration using multiple unique character sets was possible.
+
+        Custom Formatting
+        An example of a unique character set would be "{4a}", which is a custom combo of an iterator
+        of 4 characters long using lowercase letters. {5aAd} is a 5 character long iterator using
+        lowercase, uppercase, and number characters for iteration. As of right now, the current custom
+        formats available are:
+        
+        a: Lowercase Alphabet
+        A: Uppercase Alphabet
+        d: decimal numbers
+        
+        More are on the way, but the custom iterators allow any combonation of each other for more unique
+        bruteforcing.
+        
+        ID Tags
+        in official python formatting, these are the numbers before a colon that tell the format function
+        which parameter to use per format tag. Examples such as {0:04d}, which to python says that the first
+        argument will go into the format. Custom formats also work with ID Tags, but in a unique way to help
+        the program differentiate the two. an example of a custom format with an id is {0+4aA}. This says
+        that the first argument is expected to be an iterator of length 4 with uppercase and lowercase letters.
+        The advantages of using ID's are two fold: if you have a bruteforce string that you know repeats in
+        another portion of the password, then you can call the iterative number twice, meaning less false
+        positives and less passwords to generate, which means it runs faster. The other advantage is you can
+        prioritize portions of the unknown password. formats with smaller ID's will change value far less often
+        than a format with a higher value. This can make life easier as the password list continues to grow in size.
+        
+        Links:
+        [1] https://docs.python.org/3.4/library/string.html#format-string-syntax\n
+        
+        Authoring:
+        This program was built by Nicholas Cottrell (Rad10Logic)
+        April 4th, 2020"""
+
+    args = parser.parse_args()
+    setup = init_formatting(args.fstring)
+    iterative_printer(*setup, args.r)
