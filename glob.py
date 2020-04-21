2f4faf552387e53c9fb381dbc8f7c0b966bf6c4b
Nick Cottrell <nrc1mr.e3@gmail.com>
2020-04-20 22:49:37 -0400

    Fixed index in ids not being ints

--- glob.py
+++ glob.py
@@ -115,7 +115,7 @@ def init_formatting(test:str):
                     dict_box.append(uppercase)
                 elif (symbol == "d"):
                     dict_box.append(digit)
-            gen_list[i[1]] = BruteChain(int(i[3]), dict_box.copy())
+            gen_list[int(i[1])] = BruteChain(int(i[3]), dict_box.copy())
             dict_box.clear()
             # formatting custom formats to pythonic formatting
             test.replace(i[0], "{{{0[0]}:{0[1]}{0[2]}s}}".format(i))
@@ -124,13 +124,13 @@ def init_formatting(test:str):
             if i[4] == "s":
                 continue  # skip strings since they should be custom
             elif (i[4] == "d" or i[4] == "n"):
-                gen_list[i[1]] = BaseChain(10, int(i[3]))
+                gen_list[int(i[1])] = BaseChain(10, int(i[3]))
             elif (i[4] == "x" or i[4] == "X"):
-                gen_list[i[1]] = BaseChain(16, int(i[3]))
+                gen_list[int(i[1])] = BaseChain(16, int(i[3]))
             elif (i[4] == "o"):
-                gen_list[i[1]] = BaseChain(8, int(i[3]))
+                gen_list[int(i[1])] = BaseChain(8, int(i[3]))
             elif (i[4] == "b"):
-                gen_list[i[1]] = BaseChain(2, int(i[3]))
+                gen_list[int(i[1])] = BaseChain(2, int(i[3]))
     else:
         index = int()
 
