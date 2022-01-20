19befb38bb3dc1941898c724f2c5de21fcbf5a02
Rad10 <nrc1mr.e3@gmail.com>
2022-01-20 11:16:39 -0500

    Changed tool name to brutesleuth

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="stringplus",
+    name="BruteSleuth",
     version="1.3.5",
     author="Nicholas Cottrell",
     author_email="ncottrellweb@gmail.com",
@@ -26,5 +26,5 @@ setuptools.setup(
     package_dir={"": "src"},
     packages=setuptools.find_packages(where="src"),
     python_requires=">=3.6",
-    scripts=['bin/stringplus'],
+    scripts=['bin/brutesleuth'],
 )
