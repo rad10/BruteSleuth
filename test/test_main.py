8caaf3aa1adff75b535d095ea8581ed9a7647e53
RadioLogic <ncottrellweb@gmail.com>
2022-01-29 16:44:09 -0500

    Added tests for minor functions in library

--- test/test_main.py
+++ test/test_main.py
@@ -37,3 +37,25 @@ class TestBaseChain:
 def test_init_formatting(format_string, format_frame):
     result, _ = brutesleuth.init_formatting(format_string)
     assert result == format_frame
+
+
+@pytest.mark.parametrize("format_string,format_mask", [
+    ("SKY-{4aA}-{4d}", "?l?u,SKY-?1?1?1?1-?d?d?d?d"),
+    ("SKY-{4aA}-{:4d}", "?l?u,SKY-?1?1?1?1-?d?d?d?d"),
+    ("Password{2d}", "Password?d?d"),
+    ("Binary{:04b}", "01,Binary?1?1?1?1"),
+])
+def test_convert_to_mask(format_string, format_mask):
+    assert brutesleuth.convert_to_mask(format_string) == format_mask
+
+
+@pytest.mark.parametrize("format_string,real_string,expected_results", [
+    ("SKY-{:4s}-{:4s}", "SKY-Aefa-0146", "Aefa_0146"),
+    ("SKY-{:4s}-{:4d}", "SKY-Aefa-0146", "Aefa_0146"),
+    ("Password{:2s}", "Password83", "83"),
+    ("Binary{:04b}", "Binary0100", "0100")
+])
+def test_get_string_variations(format_string, real_string, expected_results):
+    results = brutesleuth.get_string_variations(format_string, real_string)
+    packing = "_".join(results)
+    assert packing == expected_results
