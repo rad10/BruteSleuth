40404bb3a3efbc8c8e0f45d3fce5dca6b0900ef6
RadioLogic <ncottrellweb@gmail.com>
2022-01-29 15:54:55 -0500

    Properly grouped tests fro basechain

--- test/test_main.py
+++ test/test_main.py
@@ -1,21 +1,26 @@
 import pytest
 import brutesleuth
-
-
 # Testing functions of Base chain chains
-@pytest.mark.parametrize(("length", "base", "expected_results"), [
-    (4, 2, [7]),
-    (3, 8, [16]),
-    (2, 16, [226]),
+
+
+@pytest.mark.parametrize("length,base", [
+    (4, 2),
+    (3, 8),
+    (2, 16),
 ])
-def test_basechain(length, base, expected_results):
-    chain = brutesleuth.BaseChain(base, length)
-    # testing length
-    assert len(chain) == base ** length
-    # testing setting a value
-    chain.setIndex(expected_results[0])
-    assert chain.__next__() == expected_results[0]
-    # testing that every iteration is correct
-    chain.setIndex(0)
-    for result, expected in zip(chain, range(base ** length)):
-        assert result == expected
+class TestBaseChain:
+    def test_length(self, length, base):
+        chain = brutesleuth.BaseChain(base, length)
+        assert len(chain) == base ** length
+
+    @pytest.mark.parametrize("index", [0, 2, 5, 12])
+    def test_set_index(self, base, length, index):
+        chain = brutesleuth.BaseChain(base, length)
+        chain.setIndex(index)
+        assert chain.__next__() == index
+
+    def test_incremental_result(self, length, base):
+        chain = brutesleuth.BaseChain(base, length)
+        chain.setIndex(0)
+        for result, expected in zip(chain, range(base ** length)):
+            assert result == expected
